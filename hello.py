cb5af1815d27aedf6e7b5bdbaa6dc0e4225e9426
Shankar Mupanna <smupanna@gmail.com>
2025-10-31 10:55:38 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,31 @@
+print("Hello, world!")
+print("i am learning python ")
+name = "shankar"
+age = 18 
+height = 44
+print(name)
+print(age)
+print(height)
+print(type(name))
+print(type(age))
+print(type(height))
+print("My name is " , name , "and i am " , age , " years old ")
+print(f"My name is {name} and i am {age} years old")
+your_name = input("Enter your name: ")
+print(f"Hello {your_name} , welcome to python ")
+a = 10
+b = 3
+print(a + b) 
+print(a-b)
+print(a / b)
+print(a//b)
+print(a%b)
+print(a ** b)
+x= 10 
+y= 20
+print(x>y)
+print(x==y)
+print(y!=x)
+print( x>5 and y<25)
+print(x>15 or y<25) 
+print(not(x>5))

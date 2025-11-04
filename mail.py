b5750654665ea22cae1192a7d79ffbda6f95f40b
Shankar Mupanna <smupanna@gmail.com>
2025-11-04 11:22:05 +0530

    Added mail.py file

---
+++ mail.py
@@ -0,0 +1,103 @@
+#excersie 
+print ("Shankar Mupanna")
+print("smupanna@gmail.com")
+name = "Shankar "
+print(name)
+#excersie 2 
+# username = input(("Enter your name: "))
+# print("Hello ",username  )
+# print(f"Hello {username }")
+# age = 18 
+# print(f"My age is { age }")
+
+#exercise 3 
+# width = float(input("Enter the width of room: "))
+# lenght = float(input("Enter the lenght of room: "))
+# area = width*lenght
+# print(area)
+# print("Area of room" , area )
+# print("Hello " , end= "") # end function that takes input from another line to same line 
+# print("Student")
+# sep function it seperates each word with hypen(-) 
+print(3, "Nov", 2025, sep="-")
+firstname= "Shankar "
+lastname= "Mupanna"
+print(f"My name is {firstname} {lastname}")
+print("My name is" ,(firstname), (lastname), sep="-")
+x= 10
+y = "greeks"
+z= 20 
+w = z
+print(x); print(y); print(z); print(w)
+w = 30 
+print(x); print(y); print(z); print(w)
+#Exercise 10: Arithmetic 
+# a = int(input("Enter integer number :"))
+# b = int(input("Enter integer number: "))
+# sum = a + b 
+# diff = b - a 
+# prod = a*b 
+# print("Sum is " , sum )
+# print ("Difference is ", diff )
+# print("Product is ", prod )
+
+a = 10 
+print(type(a))
+
+b = 10.5
+print(type(b))
+
+c = None 
+print(type(c))
+
+d = 'c'
+print(type(d))
+
+f = "abcde"
+print(type(f))
+
+z = 3 + 5j
+print(z)
+
+#exercise 1 - identify data types 
+# a = 1
+# b = 2.9 
+# c = "abc"
+# d = True 
+# e = [1, 2, 3, ]
+# f = ( d, f,)
+# g = { 3, 5, }
+# print(type (a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
+
+#exercise 2 user input type checking 
+# user = int(input("Enter number:"))
+# user2 = float(input("Enter decimal number :"))
+# user3 = input("Enter:")
+# user4 = list(input("Enter "))
+# user5 = tuple(input("Enter:"))
+# print(type(user))
+# print(type(user2 ))
+# print(type(user3 ))
+# print(type(user4))
+# print(type(user5))
+
+#exercise 3 - compare data types
+# m = True 
+# n = "False "
+# print(type(m))
+# print(type(n ))
+
+#exercise 4 - type conversion demonstration
+# a = True 
+# print(type(a))
+# a = str(a)
+# print(type(a))
+
+k  = eval(input("enter "))
+print(type(k))
